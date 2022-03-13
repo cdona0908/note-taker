c39f86e37104b06d2a2c1ddfea7faa6efe9b391b
Celia <celiamdona@gmail.com>
2022-03-12 20:51:55 -0500

    added routes to add notes to database

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {filterByQuery, findById} = require('../../lib/notes');
+const {filterByQuery, findById, createNewNote, validateNote} = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
 
@@ -24,6 +24,19 @@ router.get('/notes/:id', (req, res) => {
     }
 });
 
+// add notes to db
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be so there are no duplicate ids
+    req.body.id = notes.length.toString();
+    //if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)){
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        // add note to json file and notes array in this function
+        const note = createNewNote(req.body, notes);        
+        res.json(note);
+    }    
+});
 
 
 module.exports = router;
